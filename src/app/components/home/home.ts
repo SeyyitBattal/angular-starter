101b47b5d9fbd55b1c766d520ad8ebed43f1b8df
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2025-11-05 09:50:51 +0300

    ng-template added with ngTemplateOutlet

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
 import { products,Products } from '../products/products';
-import { Custom } from "../../custom/custom";
 import { FormsModule } from "@angular/forms";
 import { HomeChild } from "../home-child/home-child";
+import { NgTemplateOutlet } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   template: `
+
   <ul>
     @for (product of products; track $index) {
       <li>Product name: {{product.name}} <br>Price: {{product.price}}$</li><hr>
     }
   </ul>
-  <app-custom></app-custom>
+
   <input type="text" [(ngModel)] = "data">
   {{data}}
+
   <app-home-child>
     <header>ng-content HEADER</header>
     <!-- <body>ng-content BODY</body> -->
     <footer>ng-content FOOTER</footer>
     <div class="footerInfo">Footer information line</div>
     </app-home-child>
+
+    <ng-container *ngTemplateOutlet= "templateArea">
+      ng-container içeriği... 
+    </ng-container>
+    <ng-template #templateArea>
+      ng-template içeriği...
+    </ng-template>
+
   `,
   standalone: true,
-  imports: [Custom, FormsModule, HomeChild]
+  imports: [FormsModule, HomeChild, NgTemplateOutlet]
 })
 export class Home {
   products:Products[] = products;
