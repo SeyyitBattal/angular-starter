03eccc17a57aece1548bdf1a83445e93297201aa
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2025-11-05 12:00:24 +0300

    ngTemplateOutlet updated with $implicit

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -33,7 +33,8 @@ import { NgTemplateOutlet } from "@angular/common";
     </ng-template> 
     <br>
 
-  <!--Veri gönderilecekse aşağıdaki şekilde uygulanır.-->
+  <!--Veri gönderilecekse aşağıdaki 2 şekilde uygulanır.-->
+  <!--Uygulama 1-->
     <ng-container [ngTemplateOutlet]= "tempTemp" [ngTemplateOutletContext]="{numberData1: 123, numberData2:456, numberData3: 789}">
       Container area!
     </ng-container>
@@ -43,12 +44,32 @@ import { NgTemplateOutlet } from "@angular/common";
       number 2: {{number2}} <br>
       number 3: {{number3}} <br>
     </ng-template>
+  <!--Uygulama 2-->
+    <ng-container *ngTemplateOutlet="personTemplate; context: {$implicit: person}" >
+    </ng-container>
+    <ng-template #personTemplate let-person>
+      <ul>
+      @for (human of person; track $index) {
+        <li>
+        {{human.pName}} - yaş: {{human.age}}
+        </li>
+      }
+      </ul>
+    </ng-template>
 
   `,
   standalone: true,
   imports: [FormsModule, HomeChild, NgTemplateOutlet]
 })
+
 export class Home {
   products:Products[] = products;
   data : any;
+  person : any[] = [
+    {pName : "Ahmet", age : 12},
+    {pName : "Ali", age : 53},
+    {pName : "Veli", age : 43},
+    {pName : "Ayşe", age : 76},
+    {pName : "Hasan", age : 24}
+  ] 
 }
